8ef0f4f4f7c5e8f604bacaa1a4febfa208e68760
Piero De Tomi <piero.detomi@euris.it>
2020-01-22 10:38:28 +0100

    Primo commit

---
+++ TestingDemo/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestingDemo
+{
+    public class Calculator
+    {
+        public decimal Add(decimal d1, decimal d2)
+        {
+            return (d1 + d2);
+        }
+
+        public decimal Subtract(decimal d1, decimal d2)
+        {
+            return (d1 - d2);
+        }
+
+        public decimal Multiply(decimal d1, decimal d2)
+        {
+            return (d1 * d2);
+        }
+
+        public decimal Divide(decimal d1, decimal d2)
+        {
+            if (d2 == 0)
+                throw new ArgumentException("Il dividendo non può essere pari a zero", "d2");
+
+            return (d1 / d2);
+        }
+    }
+}
---
+++ TestingDemo/specifiche.md
@@ -0,0 +1,20 @@
+﻿Data una stringa in input, generare un codice composto così:
+
+```PREFIX + BODY + SUFFIX```
+
+## Generazione PREFIX
+- Se la prima lettera è una _vocale_ => ```PREFIX = "VC"```
+- Se la prima lettera è una _consonante_ => ```PREFIX = "CS"```
+
+## Generazione BODY
+Il body sarà determinato dalla concatenazione di:
+
+- ```"B"```
+- Lunghezza della stringa di input
+- Data la terza lettera dell'input:
+  - Se è una _vocale_ => ```"00--"```
+  - Se è una _consonante_ => ```"000:"```
+
+## Generazione SUFFIX
+- Se la lunghezza della stringa di input è _pari_ => ```"$PR"```
+- Se la lunghezza della stringa di input è _dispari_ => ```"$DS"```
